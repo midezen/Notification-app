28f37edced03b6fa1dd27efb6cb898b1af152b76
midezen <victordiya90@gmail.com>
2024-04-03 21:56:49 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="login">
+        <input type="text" placeholder="username" />
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
